d249c7f76d21a87911ea96ef736f798035e7484e
tori3 <kikurage8o8o@gmail.com>
2016-10-31 16:42:51 +0900

    20161024 submit

---
+++ src/calculation/Calculation_lib.java
@@ -0,0 +1,17 @@
+package calculation;
+
+public class Calculation_lib {
+	
+	private int m,n;
+
+	public Calculation_lib(int m, int n) {
+		this.m = m;
+		this.n = n;
+	}
+	
+	public int getPlus(){
+		return this.m+this.n;
+	}
+	
+
+}
